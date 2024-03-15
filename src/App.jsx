568822e085991d19058d356b9350a8332da93531
arifraj199 <karif9514@gmail.com>
2024-03-15 21:40:35 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl">Chefs Table</h1>
+      <Header></Header>
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import { CgProfile } from "react-icons/cg";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52  text-lg font-normal"
+            >
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Recipes</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Search</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-3xl font-bold">Recipe Calories</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-lg font-normal">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Recipes</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Search</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered bg-[#150b2b0d] rounded-[50px] text-[#150b2bb3] w-24 md:w-auto"
+            />
+          </div>
+          <div className=" rounded-full bg-[#0BE58A] p-2">
+            <CgProfile className="text-3xl " />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
