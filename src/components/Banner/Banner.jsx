aba9c54ab23e8702527bcefed7c7dfb4cb616707
arifraj199 <karif9514@gmail.com>
2024-03-16 14:12:58 +0600

    banner button style and responsive style added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -11,17 +11,21 @@ const Banner = () => {
       }}
       className="rounded-2xl text-white flex flex-col items-center justify-center text-center"
     >
-      <h1 className="text-5xl w-3/5">
+      <h1 className="text-3xl lg:text-[52px] font-bold lg:leading-[76px] lg:w-3/4">
         Discover an exceptional cooking class tailored for you!
       </h1>
-      <p className="text-lg font-normal w-3/5 mt-6 mb-10">
+      <p className="text-sm lg:text-lg font-extralight mx-2 lg:mx-0 lg:w-3/5 mt-4 lg:mt-6 mb-8 lg:mb-10">
         Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
         Database and solve 500+ coding problems to become an exceptionally well
         world-class Programmer.
       </p>
-      <div className="flex gap-8">
-        <button className="btn btn-accent rounded-[50px]">Explore now</button>
-        <button className="btn rounded-[50px]">Our Feedback</button>
+      <div className="flex gap-4 lg:gap-[30px]  lg:text-xl font-semibold">
+        <button className="px-4 lg:px-5 py-3 lg:py-4 bg-[#7951ff] border-0 hover:bg-none text-white rounded-[50px]">
+          Explore now
+        </button>
+        <button className="px-4 lg:px-5 py-3 lg:py-4 bg-[#FFFFFF] border-0 hover:bg-none text-black rounded-[50px]">
+          Our Feedback
+        </button>
       </div>
     </div>
   );
